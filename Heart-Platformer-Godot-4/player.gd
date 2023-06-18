62cf5d1ffead93d2597f17d5377ff95a0c045496
El-NyaX3M <niggel.x3m@gmail.com>
2023-06-18 15:58:14 -0700

    Escenario base, personaje animado y movimiento.

--- Heart-Platformer-Godot-4/player.gd
+++ Heart-Platformer-Godot-4/player.gd
@@ -1,54 +1,66 @@
 extends CharacterBody2D
 
+
 const SPEED = 100.0
 const ACCELERATION = 800.0
 const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#Se obtiene el valor de la gravedad por medio de una llamada de los settings de godot.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var coyote_jump_timer = $CoyoteJumpTimer
 
+#El método _physics_process estará corriendo 60* veces por segundo.
+#* este valor puede cambiarse en los ajustes del proyecto.
+#La variable delta funciona como un contador de tiempo entre los frames y un segundo.
+#aquí la variable delta tiene un valor de 1/60
 func _physics_process(delta):
-	apply_gravity(delta)
-	handle_jump()
-	var input_axis = Input.get_axis("ui_left", "ui_right")
-	handle_acceleration(input_axis, delta)
-	apply_friction(input_axis, delta)
-	update_animations(input_axis)
-	var was_on_floor = is_on_floor()
-	move_and_slide()
-	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
-	if just_left_ledge:
-		coyote_jump_timer.start()
-
-func apply_gravity(delta):
+	# Add the gravity.
+	#is_on_floor valida que hayan nodos estáticos en el lado opuesto a la propiedad Up Direction
+	#en este caso, Up Direction tiene valor 0,-1 (arriba) asi que el suelo debe estar en 0,1 (abajo)
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-func handle_jump():
-	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("ui_up"):
+	# Handle Jump.
+	#Input hace referencia a lo que se ingrese por medio del teclado
+	#Se valida si se presiona la barra espaciadora y si se está en el suelo.
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
-	if not is_on_floor():
-		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
+	else:
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 
-func handle_acceleration(input_axis, delta):
-	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
-
-func apply_friction(input_axis, delta):
-	if input_axis == 0:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#La dirección se obtendrá de acuerdo a la tecla que se presione
+	#flecha izquierda (-1) o flecha derecha (1)
+	#Input.get_axis(<left>, <right>)
+	var direction = Input.get_axis("ui_left", "ui_right")
+	#Si hay valor en la variable direction entonces le asigna a la propiedad x de velocity una velocidad
+	#esta velocidad es obtenida multiplicando la direction (-1 o 1) por SPEED (definido como 200 al inicio).
+	#En caso contrario entonces se le asigna 0.
+	if direction:
+		#velocity.x = direction * SPEED
+		#move_toward hace que el objeto se mueva, ingresando su posición inicial, su velocidad final y 
+		#el cambio de velocidad respecto a delta
+		velocity.x = move_toward(velocity.x, SPEED * direction, delta * ACCELERATION)
+	else:
+		#velocity.x = 0
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-
-func update_animations(input_axis):
-	if input_axis != 0:
-		animated_sprite_2d.flip_h = (input_axis < 0)
+	
+	update_animations(direction)
+	#move_and_slide permite hacer el movimiento del objeto validando colisiones con nodos estáticos
+	#este toma la variable velocity y hace el resto del trabajo.
+	move_and_slide()
+	
+func update_animations(direction):
+	if direction:
+		animated_sprite_2d.flip_h = (direction < 0)
 		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("idle")
-	
+		
 	if not is_on_floor():
 		animated_sprite_2d.play("jump")
--- Heart-Platformer-Godot-4/world.gd
+++ Heart-Platformer-Godot-4/world.gd
@@ -1,8 +1,14 @@
 extends Node2D
+# Para comentar se usa el caracter Hash
 
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+#@onready indica que esa variable se inicializa al correr el programa
+#para asignar un objeto nodo a una variable se ingresa la dirección del objeto como es a continuación:
+@onready var polygon_2d = $Zone/CollisionPolygon2D/Polygon2D
+#Esta último comando puede ahorrarse al arrastrar el objeto deseado + ctrl hacia el editor del codigo
+@onready var collision_polygon_2d = $Zone/CollisionPolygon2D
 
+#Las funciones se declaran con 'func' + <nombre_de_la_función> + ():
+#La función _ready(): corre cuando carga el nodo al que pertenece se encuentra listo (cuando inicia el juego)
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	polygon_2d.polygon = collision_polygon_2d.polygon
+	polygon_2d.polygon = collision_polygon_2d.polygon #asigna los poligonos del objeto a los poligonos de otro
